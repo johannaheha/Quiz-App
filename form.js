c3de14c362cb62186f4afe361e92f19b40cc00c4
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-20 16:12:18 +0200

    Add html for new Card

---
+++ form.js
@@ -0,0 +1,3 @@
+const questionInput = document.querySelector('[data-js="question"]');
+const answerInput = document.querySelector('[data-js="answer"]');
+const tagInput = document.querySelector('[data-js="tag"]');
