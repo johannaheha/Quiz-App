b11746b6c6bcf1f7320987a78c6a4f0ee0a79d90
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-20 16:39:33 +0200

    adding questions

--- form.js
+++ form.js
@@ -1,3 +1,23 @@
+//selektiert das Formular und die Input-Felder
+
+const form = document.querySelector('[data-js="form"]');
 const questionInput = document.querySelector('[data-js="question"]');
 const answerInput = document.querySelector('[data-js="answer"]');
 const tagInput = document.querySelector('[data-js="tag"]');
+
+//EventListener fürs Absenden des Formulars
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); //verhindert das Neuladen
+
+    //Werte auslesen
+    const question = questionInput.value;
+    const answer = answerInput.value;
+    const tag = tagInput.value;
+
+    //Werte ausgeben (zum Testen)
+    console.log("Question:", question);
+    console.log("Answer:", answer);
+    console.log ("Tag:", tag);
+})
+
