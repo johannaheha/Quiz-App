7f3770187684e31be3b67dd189657173e9031c8e
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-06-20 23:02:51 +0200

    add dynamic card

--- form.js
+++ form.js
@@ -4,6 +4,7 @@ const form = document.querySelector('[data-js="form"]');
 const questionInput = document.querySelector('[data-js="question"]');
 const answerInput = document.querySelector('[data-js="answer"]');
 const tagInput = document.querySelector('[data-js="tag"]');
+const cardContainer = document.querySelector('[data-js="card-container"]');
 
 //EventListener fürs Absenden des Formulars
 
@@ -19,5 +20,21 @@ form.addEventListener("submit", (event) => {
     console.log("Question:", question);
     console.log("Answer:", answer);
     console.log ("Tag:", tag);
-})
-
+    
+    // Karte erzeugen
+    
+    const newCard = document.createElement("article");
+    newCard.classList.add("questioncard");
+    newCard.innerHTML = `<h2 class="questioncard__question">${question}</h2>
+    <button class="questioncard__button" aria-label="show answer">show answer</button>
+    <p class="questioncard__answer questioncard__answer--hidden">${answer}</p>
+    <i class="questioncard__bookmark ph ph-bookmark-simple"></i>
+    <ul class="questioncard__categories">
+    <li class="questioncard__category-item">${tag}</li>
+    </ul>`;
+    
+    // Karte einfügen
+    
+    cardContainer.appendChild(newCard);
+      form.reset();
+});
